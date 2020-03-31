09d1667a7e608ea22f469466358f9a50f53a39e9
tom54007 <752138607@qq.com>
2020-03-31 17:46:17 +0800

    初始化提交

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,15 @@
+# flake8: NOQA
+from .base import *
+
+DEBUG = True
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
